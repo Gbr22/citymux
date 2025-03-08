58ae1506e8f873c02636df13d9555f15007b6d2d
Gabor Krisko <git@krisko.dev>
2025-03-08 20:30:11 +0100

    fix layout size rounding

--- src/draw.rs
+++ src/draw.rs
@@ -57,38 +57,62 @@ pub fn find_span_dimensions(node: &Node, span_id: usize, parent_dimensions: Rect
             for child in &span.children {
                 total += child.size;
             }
-            let mut last_size = Vector2::new(0, 0);
-            let mut last_position = parent_dimensions.position;
 
-            let mut i = 0;
-            for child in &span.children {
-                let is_last = i == span.children.len() - 1;
-                i += 1;
+            let mut sizes = vec![Vector2::default(); span.children.len()];
+            let mut remaining_size = parent_dimensions.size;
+            for (index, child) in span.children.iter().enumerate() {
                 let size = child.size;
                 let ratio = size / total;
-                let size = if is_last {
-                    match direction {
-                        SpanDirection::Horizontal => Vector2::new(
-                            (parent_dimensions.size.x as f64 * ratio).ceil() as isize,
-                            parent_dimensions.size.y,
-                        ),
-                        SpanDirection::Vertical => Vector2::new(
-                            parent_dimensions.size.x,
-                            (parent_dimensions.size.y as f64 * ratio).ceil() as isize,
-                        ),
-                    }
-                } else {
-                    match direction {
-                        SpanDirection::Horizontal => Vector2::new(
-                            (parent_dimensions.size.x as f64 * ratio).round() as isize,
-                            parent_dimensions.size.y,
-                        ),
-                        SpanDirection::Vertical => Vector2::new(
-                            parent_dimensions.size.x,
-                            (parent_dimensions.size.y as f64 * ratio).floor() as isize,
-                        ),
-                    }
+                let size = match direction {
+                    SpanDirection::Horizontal => Vector2::new(
+                        (parent_dimensions.size.x as f64 * ratio).floor() as isize,
+                        parent_dimensions.size.y,
+                    ),
+                    SpanDirection::Vertical => Vector2::new(
+                        parent_dimensions.size.x,
+                        (parent_dimensions.size.y as f64 * ratio).floor() as isize,
+                    ),
                 };
+                sizes[index] = size;
+                remaining_size = remaining_size - size;
+            }
+            match direction {
+                SpanDirection::Horizontal => {
+                    while remaining_size.x > 0 {
+                        let smallest = sizes
+                            .iter_mut()
+                            .enumerate()
+                            .min_by_key(|(_, size)| size.x);
+                        let Some(smallest) = smallest else {
+                            break;
+                        };
+                        let smallest = smallest.0;
+
+                        sizes[smallest].x += 1;
+                        remaining_size.x -= 1;
+                    }
+                },
+                SpanDirection::Vertical => {
+                    while remaining_size.y > 0 {
+                        let smallest = sizes
+                            .iter_mut()
+                            .enumerate()
+                            .min_by_key(|(_, size)| size.y);
+                        let Some(smallest) = smallest else {
+                            break;
+                        };
+                        let smallest = smallest.0;
+
+                        sizes[smallest].y += 1;
+                        remaining_size.y -= 1;
+                    }
+                },
+            }
+
+            let mut last_size = Vector2::new(0, 0);
+            let mut last_position = parent_dimensions.position;
+            for (index, child) in span.children.iter().enumerate() {
+                let size = sizes[index];
                 let position = match direction {
                     SpanDirection::Horizontal => {
                         Vector2::new(last_position.x + last_size.x, last_position.y)
